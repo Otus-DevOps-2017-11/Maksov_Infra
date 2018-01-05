8d2ce7820a72c2d38915fb102e41663515f1f035
Maks Ovchinnikov <classic12@mail.ru>
2018-01-05 15:27:50 +0500

    Задание** Создание балансировщика с одним инстансом

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,36 @@ provider "google" {
   region  = "${var.region}-d"
 }
 
+resource "google_compute_project_metadata" "sshkey1" {
+
+  metadata  {
+
+    ssh-keys  = "Maksim:${file("C:/Users/Maksim/.ssh/Maksim.pub")}"
+
+  }
+
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с тегом
+    target_tags = ["reddit-app"]
+
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
@@ -15,8 +45,6 @@ resource "google_compute_instance" "app" {
       image = "${var.disk_image}"
     }
   }
-
-
 
   tags = ["reddit-app"]
 
@@ -46,33 +74,68 @@ resource "google_compute_instance" "app" {
   }
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+resource "google_compute_global_address" "global_ip" {
+  name = "global-reddit-ip"
+}
 
-  # Название сети, в которой действует правило
-  network = "default"
+resource "google_compute_http_health_check" "reddit-health" {
+  name         = "reddit-health-check"
+  port = "9292"
+  timeout_sec        = 1
+  check_interval_sec = 1
+}
 
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
+resource "google_compute_instance_group" "webservers" {
+  name        = "reddit-group"
+  description = "Reddit instance group"
+
+  instances = [
+    "${google_compute_instance.app.self_link}"
+  ]
+
+  named_port {
+    name = "reddit-group-port"
+    port = "9292"
   }
 
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с тегом
-    target_tags = ["reddit-app"]
+  zone = "${var.zone}-d"
 
 }
 
+resource "google_compute_backend_service" "reddit-bk" {
 
-resource "google_compute_project_metadata" "sshkey1" {
+  name        = "reddit-backend"
+  description = "Our company website"
+  port_name   = "reddit-group-port"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  enable_cdn  = false
 
-  metadata  {
+  backend {
+    group = "${google_compute_instance_group.webservers.self_link}"
+  }
 
-    ssh-keys  = "Maksim1:${file("C:/Users/Maksim/.ssh/Maksim.pub")}\n Maksim2:${file("C:/Users/Maksim/.ssh/Maksim.pub")}"
+  health_checks = ["${google_compute_http_health_check.reddit-health.self_link}"]
+
+}
+
+resource "google_compute_url_map" "reddit-map" {
+  name        = "urlmap"
+  description = "a description"
+
+  default_service = "${google_compute_backend_service.reddit-bk.self_link}"
 
   }
 
+resource "google_compute_target_http_proxy" "http-reddit-proxy" {
+    name        = "reddit-proxy"
+    description = "a description"
+    url_map     = "${google_compute_url_map.reddit-map.self_link}"
+  }
+
+resource "google_compute_global_forwarding_rule" "default" {
+    name       = "default-rule"
+    target     = "${google_compute_target_http_proxy.http-reddit-proxy.self_link}"
+    ip_address = "${google_compute_global_address.global_ip.address}"
+    port_range = "80"
 }
