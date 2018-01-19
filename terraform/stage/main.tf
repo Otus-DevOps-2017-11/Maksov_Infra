595393ff293833249c75d62e834e1ef23ae58040
Maks Ovchinnikov <classic12@mail.ru>
2018-01-19 17:22:37 +0500

    Основное ДЗ № 12

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -45,7 +45,7 @@ module "app" {
   ports_app = ["9292"]
   source_ranges = "${var.source_ranges_default}"
   target_tags = ["reddit-app"]
-  ip_db  = "${module.db.app_external_ip}"
+  ip_db  = "${module.db.db_external_ip}"
 }
 
 module "vpc" {
