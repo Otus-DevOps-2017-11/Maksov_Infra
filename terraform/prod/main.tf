2850fdf313178632cc3bcfde8e7e2af9cc522897
Maks Ovchinnikov <classic12@mail.ru>
2018-01-24 03:08:39 +0500

    Разрешение конфликтов 3

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -44,7 +44,7 @@ module "app" {
   ports_app = ["9292"]
   source_ranges = "${var.source_ranges_default}"
   target_tags = ["reddit-app"]
-  ip_db  = "${module.db.db_external_ip}"
+
 }
 
 module "vpc" {
